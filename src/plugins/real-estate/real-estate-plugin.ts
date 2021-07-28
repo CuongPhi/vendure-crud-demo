3ed6a48f81b445e6f73a91d62db76aaeae7f1d92
Cuong dev <nficuong@gmail.com>
2021-07-28 23:12:26 +0700

    demo cron shedule

--- src/plugins/real-estate/real-estate-plugin.ts
+++ src/plugins/real-estate/real-estate-plugin.ts
@@ -2,16 +2,17 @@ import { PluginCommonModule, VendurePlugin } from '@vendure/core';
 import { RealEstate } from './entities/real-estate.entity';
 import { adminApiExtensions } from './api/api-extensions';
 import { RealEstateService } from "./service/real-estate.service";
+import { CronScheduleService } from "./service/cron-schedule.service";
 import { AdminUiExtension } from '@vendure/ui-devkit/compiler';
 import { RealEstateAdminResolver } from './api/admin-api-resolver';
 
 import path from "path";
 import {RealEstateSharedModule} from "./ui/real-estate-shared.module";
-
+import { ScheduleModule } from '@nestjs/schedule';
 
 
 @VendurePlugin({
-    imports: [ PluginCommonModule ],
+    imports: [ PluginCommonModule, ScheduleModule.forRoot() ],
     entities: [ RealEstate ],
     adminApiExtensions: {
         schema: adminApiExtensions,
@@ -19,6 +20,7 @@ import {RealEstateSharedModule} from "./ui/real-estate-shared.module";
     },
     providers: [
         RealEstateService,
+        CronScheduleService,
         // By definiting the `PLUGIN_INIT_OPTIONS` symbol as a provider, we can then inject the
         // user-defined options into other classes, such as the {@link ExampleService}.
        // { provide: PLUGIN_INIT_OPTIONS, useFactory: () => FeedbackPlugin.options },
@@ -26,6 +28,7 @@ import {RealEstateSharedModule} from "./ui/real-estate-shared.module";
 })
 
 export class RealEstatePlugin {
+
     static uiExtensions: AdminUiExtension = {
         ngModules: [
             {
